e6b2139edc7dbc6e0c95bd2a012af66fca2fefcb
tiagobr21 <tiagobrr17@gmail.com>
2022-07-22 13:37:31 -0300

    Select Missa/Celebrção

--- routes/escala_coroinha.js
+++ routes/escala_coroinha.js
@@ -7,73 +7,192 @@ var checkRole = require('../services/checkRole');
 
 router.post('/create',(req,res)=>{
   let escala_coroinha = req.body;
-  let query_consulta=('select missa,data,dia,hora,comunidade,acolito1,acolito2,acolito3,coroinha1,coroinha2,coroinha3,coroinha4,coroinha5 from escala_coroinha where id=?');
+  let query_consulta=('select missa,data,mes,dia,hora,comunidade,acolito1,acolito2,acolito3,coroinha1,coroinha2,coroinha3,coroinha4,coroinha5 from escala_coroinha where id=?');
   connection.query(query_consulta,[escala_coroinha.missa],(err,results)=>{
-    if(!err){
-        if(results.length <= 0){
-          if(escala_coroinha.missa === 'Domingo: Missa/Celebração Santa Teresinha 7h'){
-            let query_criar = ("insert into escala_coroinha values(default,?,?,'Domingo','7h','Santa Terezinha',?,?,?,?,?,?,?,?)");
-             connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.acolito1,escala_coroinha.acolito2,
-              escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+      if(!err){
+            if(results.length <= 0){
+              if(escala_coroinha.missa === 'Domingo: Missa/Celebração Santa Teresinha 7h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Domingo','7h','Santa Terezinha',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
                 (err,results)=>{
-                 
-                 if(!err){
+                  if(!err){
                     res.status(200).json({message:'Escala Cadastrada com sucesso'});
-                }else{
+                  }else{
                     res.status(500).json(err);
-                }
-            });
-        }else if(escala_coroinha.missa === 'Domingo: Missa/Celebração N. S. Rosário 7h'){
-          let query_criar = ("insert into escala_coroinha values(default,?,?,'Domingo','7h','N. S. Rosário',?,?,?,?,?,?,?,?)");
-          connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.acolito1,escala_coroinha.acolito2,
-           escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
-             (err,results)=>{
-              
-              if(!err){
-                 res.status(200).json({message:'Escala Cadastrada com sucesso'});
-             }else{
-                 res.status(500).json(err);
-             }
-         });
-        }else if(escala_coroinha.missa === 'Domingo: Missa/Celebração N. S. Perpétuo Socorro 8h30'){
-          let query_criar = ("insert into escala_coroinha values(default,?,?,'Domingo','8h30','N. S. Perpétuo Socorro',?,?,?,?,?,?,?,?)");
-          connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.acolito1,escala_coroinha.acolito2,
-           escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
-             (err,results)=>{
-              
-              if(!err){
-                 res.status(200).json({message:'Escala Cadastrada com sucesso'});
-             }else{
-                 res.status(500).json(err);
-             }
-         });
-        }else if(escala_coroinha.missa === 'Domingo: Missa/Celebração  N. S. Rosário 17h'){
-          let query_criar = ("insert into escala_coroinha values(default,?,?,'Domingo','17h','N. S. Rosário',?,?,?,?,?,?,?,?)");
-          connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.acolito1,escala_coroinha.acolito2,
-           escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
-             (err,results)=>{
-              
-              if(!err){
-                 res.status(200).json({message:'Escala Cadastrada com sucesso'});
-             }else{
-                 res.status(500).json(err);
-             }
-         });
-    }
-    else{
-        res.status(500).json(err);
-    }
-    }
-  });
-});
+                  }
+                });
+              }else if(escala_coroinha.missa === 'Domingo: Missa/Celebração N.S.Rosário 7h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Domingo','7h','N.S.Rosário',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                  (err,results)=>{
+                    
+                    if(!err){
+                      res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                  }else{
+                      res.status(500).json(err);
+                  }
+              });
+              }else if(escala_coroinha.missa === 'Domingo: Missa/Celebração N.S.Perpétuo Socorro 8h30'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Domingo','8h30','N.S.Perpétuo Socorro',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                  (err,results)=>{
+                    if(!err){
+                      res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                    }else{
+                      res.status(500).json(err);
+                    }
+                  });
+              }else if(escala_coroinha.missa === 'Domingo: Missa/Celebração N.S.Rosário 17h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Domingo','17h','N.S.Rosário',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                  (err,results)=>{
+                    if(!err){
+                      res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                    }else{
+                      res.status(500).json(err);
+                    }
+                  });
+              }else if(escala_coroinha.missa === 'Domingo: Missa/Celebração Santa Teresinha 19h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Domingo','17h','N.S.Rosário',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                  (err,results)=>{
+                    if(!err){
+                      res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                    }else{
+                      res.status(500).json(err);
+                    }
+                  });
+              }else if(escala_coroinha.missa === 'Terça: Novena com Adoração e Celebração Eucarística Santa Teresinha 19h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Terça','19h','Santa Teresinha',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                  (err,results)=>{
+                    if(!err){
+                        res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                    }else{
+                        res.status(500).json(err);
+                    }
+                  });
+              }else if(escala_coroinha.missa === 'Terça: Novena com Adoração e Celebração Eucarística São Pedro 19h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Terça','19h','São Pedro',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                  (err,results)=>{
+                    if(!err){
+                        res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                    }else{
+                        res.status(500).json(err);
+                    }
+                  });
+              }else if(escala_coroinha.missa === 'Terça: Novena com Adoração e Celebração Eucarística N. S. Perpétuo Socorro 19h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Terça','19h','N. S. Perpétuo Socorro',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                 escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                   (err,results)=>{
+                      if(!err){
+                        res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                      }else{
+                        res.status(500).json(err);
+                      }
+                    });
+              }else if(escala_coroinha.missa === 'Quarta: Terço dos Homens Ministração da Palavra de Deus Santa Teresinha 19h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Quarta','19h','Santa Teresinha',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                 escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                   (err,results)=>{
+                      if(!err){
+                        res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                      }else{
+                        res.status(500).json(err);
+                      }
+                    });
+              }else if(escala_coroinha.missa === 'Quarta: Terço dos Homens Ministração da Palavra de Deus Santa Teresinha 19h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Quarta','19h','Santa Teresinha',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                 escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                   (err,results)=>{
+                      if(!err){
+                        res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                      }else{
+                        res.status(500).json(err);
+                      }
+                    });
+              }else if(escala_coroinha.missa === 'Quinta: Novena com Adoração e Celebração Eucarística N.S.Rosário 19h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Quinta','19h','N.S.Rosário',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                 escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                   (err,results)=>{
+                      if(!err){
+                        res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                      }else{
+                        res.status(500).json(err);
+                      }
+                    });
+              }else if(escala_coroinha.missa === 'Quinta: Adoração ao Santíssimo Grupo de Oração Santa Teresinha 20h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Quinta','20h','Santa Teresinha',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                 escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                   (err,results)=>{
+                      if(!err){
+                        res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                      }else{
+                        res.status(500).json(err);
+                      }
+                    });
+              }else if(escala_coroinha.missa === 'Sábado: Missa/Celebração São Pedro 19h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Sábado','19h','São Pedro',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                 escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                   (err,results)=>{
+                      if(!err){
+                        res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                      }else{
+                        res.status(500).json(err);
+                      }
+                    });
+              }else if(escala_coroinha.missa === 'Sábado: Missa/Celebração Sagrado Coração de Jesus 19h'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Sábado','19h','Sagrado Coração de Jesus',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                 escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                   (err,results)=>{
+                      if(!err){
+                        res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                      }else{
+                        res.status(500).json(err);
+                      }
+                    });
+              }else if(escala_coroinha.missa === 'Sábado: Missa em Reparação ao Imaculado Coração de Maria Santa Teresinha 17h45'){
+                let query_criar = ("insert into escala_coroinha values(default,?,?,?,'Sábado','17h45','Santa Teresinha',?,?,?,?,?,?,?,?)");
+                connection.query(query_criar,[escala_coroinha.missa,escala_coroinha.data,escala_coroinha.mes,escala_coroinha.acolito1,escala_coroinha.acolito2,
+                 escala_coroinha.acolito3,escala_coroinha.coroinha1,escala_coroinha.coroinha2,escala_coroinha.coroinha3,escala_coroinha.coroinha4,escala_coroinha.coroinha5],
+                   (err,results)=>{
+                      if(!err){
+                        res.status(200).json({message:'Escala Cadastrada com sucesso'});
+                      }else{
+                        res.status(500).json(err);
+                      }
+                    });
+              }
+            
+            }else{
+                res.status(500).json(err);
+            }//results.length
+      }//!err
+  });//CONNECTION
+});//ROUTER
 
 router.get('/read',(req,res)=>{
   let escala_coroinha = req.body;
-  let query_read=('select missa,data,dia,hora,comunidade,acolito1,acolito2,acolito3,coroinha1,coroinha2,coroinha3,coroinha4,coroinha5 from escala_coroinha where id=?');
+  let query_read=('select missa,data,mes,dia,hora,comunidade,acolito1,acolito2,acolito3,coroinha1,coroinha2,coroinha3,coroinha4,coroinha5 from escala_coroinha where id=?');
   connection.query(query_read,[escala_coroinha.missa],(err,results)=>{
     if(!err){
         if(results.length <= 0){
-            let query = ('select id,missa,data,dia,hora,comunidade,acolito1,acolito2,acolito3,coroinha1,coroinha2,coroinha3,coroinha4,coroinha5 from escala_coroinha');
+            let query = ('select id,missa,data,mes,dia,hora,comunidade,acolito1,acolito2,acolito3,coroinha1,coroinha2,coroinha3,coroinha4,coroinha5 from escala_coroinha order by id DESC');
             connection.query(query,(err,results)=>{
                 if(!err){
                     res.status(200).json(results);
@@ -135,18 +254,6 @@ router.put('/update/:id',(req,res)=>{
   });
   });
 
-
-router.get("/search",(req,res)=>{
-  let escala_coroinha = req.body
-  let query="select id,missa,local,data,pessoas from escala_coroinha where missa=?"
-  connection.query(query,[escala_coroinha.missa],(err,results)=>{
-    if(!err){
-      res.status(200).json(results);
-    }else{
-      res.status(500).json(err);
-    }
-  });
-});
 
 router.get("/select_coroinha",(req,res)=>{
     let coroinhas = req.body
