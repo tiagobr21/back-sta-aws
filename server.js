016152bba7c715eb0d7a7951665eeb9d664ea636
tiagosouza <tiagosouza@fametro.edu.br>
2023-03-09 16:49:11 -0400

    Adequacao para deploy no Heroku

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
-// importar express
-const express = require('express');
-//iniciar express
-const app = express();
-// nome da pasta no dist que será feito o build
-const appName = 'back-sta';
-// local onde build ira gerar os arquivos
-const outputPath = `${__dirname}/dist/${appName}`;
-
-// seta o diretorio de build para servir o conteudo Angular
-app.use(express.static(outputPath));
-
-app.get('/*',(req,res)=>{
-    res.sendFile(`${outputPath}/index`);
-});
-
-//ouvir a porta que o Heroku disponibilizar 
-app.listen(process.env.PORT);
+require('dotenv').config();
+const http = require('http');
+const app = require('./index');
+const server = http.createServer(app);
+server.listen(process.env.PORT);
