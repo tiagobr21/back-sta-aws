cb143281ad16f483cc5a34f3a9b3425005fbfdf3
tiagosouza <tiagosouza@fametro.edu.br>
2023-03-10 09:42:40 -0400

    update

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-var db_config  = mysql.createConnection({
+var connection  = mysql.createConnection({
    port: process.env.DB_PORT,
    host: process.env.DB_HOST,
    user: process.env.DB_USERNAME,
@@ -11,31 +11,14 @@ var db_config  = mysql.createConnection({
 });
 
 
+connection.connect((err)=>{
+  if(!err){
+   console.log("Connected");
+  }else{
+   console.log(err);
+  }
+});
 
-function handleDisconnect() {
-  let connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                   // the old one cannot be reused.
- 
-   connection.connect(function(err) {              // The server is either down
-     if(err) {                                     // or restarting (takes a while sometimes).
-       console.log('error when connecting to db:', err);
-       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-     }else{
-       console.log("Connected");
-     }                                     // to avoid a hot loop, and to allow our node script to
-   });                                     // process asynchronous requests in the meantime.
-                                           // If you're also serving http, display a 503 error.
-   connection.on('error', function(err) {
-     console.log('db error', err);
-     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-       handleDisconnect();                         // lost due to either server restart, or a
-     } else {                                      // connnection idle timeout (the wait_timeout
-       throw err;                                  // server variable configures this)
-     }
-   });
-
-
- }
 
  module.exports = connection;
 
