b19d214ed98911ab2adfcd0b9ac31947ec4eb9ca
tiagosouza <tiagosouza@fametro.edu.br>
2023-03-10 09:36:51 -0400

    update

--- connection.js
+++ connection.js
@@ -23,15 +23,7 @@ var db_config  = mysql.createConnection({
      }                                     // to avoid a hot loop, and to allow our node script to
    });                                     // process asynchronous requests in the meantime.
                                            // If you're also serving http, display a 503 error.
-   connection.on('error', function(err) {
-     console.log('db error', err);
-     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-       handleDisconnect();                         // lost due to either server restart, or a
-     } else {                                      // connnection idle timeout (the wait_timeout
-       throw err;                                  // server variable configures this)
-     }
-   });
- 
+
 
 module.exports = connection;
 
